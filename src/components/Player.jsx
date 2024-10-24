10af478f48e778fca993d1a0321f37e1c6977ea2
hiTech <braykul2605@gmail.com>
2024-10-24 09:48:21 +0700

    Upload structure source

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className='h-[10] bg-black flex items-center justify-between p-4'>
+
+    </div>
+  )
+}
+
+export default Player
