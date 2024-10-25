b73622e0d026b6f8267e306cfa746d892fd95f36
hiTech <braykul2605@gmail.com>
2024-10-25 14:20:37 +0700

    Structure folder

---
+++ spotify-backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// app config
+const app = express()
+const port = process.env.PORT || 3001
+
+
+// middleware
+app.use(express.json())  
+app.use(cors())
